6211ba06db3c7a747fbfd27428aaab1a99052491
mmorgan <morganwebdevelopment@gmail.com>
2020-11-07 11:52:47 -0500

    created working display of user and amounts.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import orderData from './users.json';
 
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userID: 1122,
+      firstName: "john",
+      lastName: "doe",
+      purchases: [
+        {date: "2020-01-23T18:25:43.511Z", amount: 140},
+        {date: "2020-02-23T18:25:43.511Z", amount: 110},
+      ]
+    };
+  }
 
+  render() {
 
+    const points = this.state.purchases.map((purchase) =>
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+      var twoPoints = math.floor( {purchase.amount })
+      <li> {purchase.amount}</li>
+    );
+    return (
+      <div>
+        <h1> Welcome {this.state.firstName} </h1>
+        <ul>
+          {items}
+        </ul>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Users />, document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
